d48a2d43e33a54e4fd01dcef3152609b08d0d0ec
Monday_David_Sarka <david_md@hamari.agency>
2023-09-07 18:24:09 +0100

    Added functionality for users to signup to mysql DB

--- server/connection_db.js
+++ server/connection_db.js
@@ -1,1 +1,12 @@
-const mysql = require('mysql');
+const mysql = require("mysql2");
+const dotenv = require("dotenv").config();
+
+const dbConnection = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.DATABASE
+  }).promise();
+
+module.exports = { dbConnection };
--- server/index.js
+++ server/index.js
@@ -1,49 +1,56 @@
-const express = require('express');
-const router = express.Router()
-require("dotenv").config()
+const express = require("express");
+const router = express.Router();
+require("dotenv").config();
+const { dbConnection, dbConStatus } = require("./connection_db");
+const bcrypt = require("bcrypt");
 
-// const dotenv = require("dotenv")
-// dotenv.config()
-// var upload = multer();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
-
 app.use(express.json());
-// app.use(upload.array());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send("Server is running perfectly")
-    console.log("Server is running perfectly")
-})
+app.get("/", (req, res) => {
+  res.send("Working Fine Bro....");
+});
 
-app.get('/signup', (req,res) => {
-    // res.render("signup")
-    console.log("Initiating A registration...");
-    res.send("Signup is running perfectly")
-})
+app.post("/signup", async (req, res) => {
+  // Get and hash the user passwords and store in a MYSQL db
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = { name: req.body.username, password: hashedPassword, address: req.body.address };
+    await dbConnection.query(
+      "INSERT INTO users(names, passwords, address) VALUES(?, ?, ?)",
+      [user.name, user.password, user.address]
+    );
+    res.send("User created");
+  } catch (error) {
+    console.log("🚀 ~ file: index.js:29 ~ app.post ~ error:", error);
+    res.send("Something went wrong.");
+  }
+});
 
-app.post('/signup', (req,res) => {
-    // Loging the user details
-        
-    const { username, password, conPassword, address } = req.body;
-    console.log(req.body);
-    res.send("Sent Successfully Dave");
-    // res.send({username, password, conPassword, address}) 
-})
+app.post("/login", async (req, res) => {
+  const user = dbConnection.find((user) => user.name == req.body.username);
+  if (user == null) {
+    res.status(400).send("No user Found brov!");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      // res.send("/dashboard");
+      res.send("Render Dashboard if responding instead of send");
+      //res.redirect("/dashbo")
+    } else {
+      res.send("Not Allowed Oga");
+    }
+  } catch (error) {
+    console.log("🚀 ~ file: index.js:46 ~ app.post ~ error:", error);
+    // res.status(500).send("Dashboard not found oga!");
+  }
+});
 
-router.use((req, res, next) => {
-    if (!req.headers['x-auth']) return next('router')
-    next()
-  })
-  
-  app.get('/user/:id', (req, res) => {
-    res.send(`'hello, user!', ${req.params.id}`)
-  })
-  
-  app.listen(port, () => {
-    console.log('server listening on port: ', port)
-    })
+app.listen(port, () => {
+  console.log("server listening on port: ", port);
+});
