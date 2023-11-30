0e9309e17485cebab3736348eaaf1564b22af872
Monday_David_Sarka <davidsarka242@gmail.com>
2023-11-30 17:37:30 +0100

    Used useState hook for names logically

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from "next/navigation"
 
 export default function page() {
+  const [name, setName] = useState("")
   const router = useRouter();
   useEffect(() => {
     fetch('http://localhost:5001/dashboard', {
@@ -19,26 +20,26 @@ export default function page() {
         alert("Session Expired, Please Login.")
         router.push("/login")
       } else{
-        document.getElementById("name").innerHTML = data.name
+        setName(data.name)
       }
     });
   }, []);
 
-  // const handleLogout = () => {
-  //   alert("Are you sure you want to log out?");
-  //   window.localStorage.removeItem('token')
-  // }
-
+  const handleLogout = () => {
+    alert("Are you sure you want to log out?");
+    window.localStorage.removeItem('token')
+  }
+  
   // React context API
 
   return (
     <div className=' min-h-[calc(100vh-7vh)] pt-2'>
     <div className='container mx-auto px-8'>
       <div className='bg-brown-bg mt-32 p-4 rounded max-w-[500px] mx-auto'>
-        <h2 className='text-center font-bold py-4'>Welcome to your Dashboard <span id="name"></span></h2>
+        <h2 className='text-center font-bold py-4'>Welcome to your Dashboard {name}</h2>
       </div>
       <div className='text-center mt-9'>
-        <Link href='/login'  className='cursor-pointer hover:bg-[#e17800] bg-[#e16800] p-4'>Logout</Link>
+        <Link href='/login' onClick={handleLogout} className='cursor-pointer hover:bg-[#e17800] bg-[#e16800] p-4'>Logout</Link>
       </div>
     </div>
   </div>
