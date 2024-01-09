a727cebf5b82c09557651759b05d51f06f73d725
Monday_David_Sarka <davidsarka242@gmail.com>
2024-01-09 23:44:57 +0100

    Populated cart Page

--- client/app/cart/page.tsx
+++ client/app/cart/page.tsx
@@ -1,11 +1,37 @@
-import { useContext } from 'react';
-import { CartContext } from "./AuthContext/CartContext";
+"use client";
+import { useContext } from "react";
+import { CartContext } from "../AuthContext/CartContext";
 
 export default function page() {
   const { cartItems, addToCart, removeFromCart, clearCart, getCartTotal } = useContext(CartContext);
   return (
-    <div>
-      
+    <div className=" min-h-[calc(100vh-7vh)] pt-12">
+      <div className="container mx-auto px-8">
+        <div className="bg-brown-bg mt-32 p-4 rounded max-w-[500px] mx-auto">
+          {cartItems.length > 0 ? (
+            <>
+              <p>Total: {getCartTotal()}</p>
+              <div className="flex justify-between">
+                <button onClick={clearCart} className="cart-btn">
+                  Clear Cart
+                </button>
+                <button
+                  onClick={() => {
+                    alert("Order submitted!"), clearCart();
+                  }}
+                  className="cart-btn"
+                >
+                  Checkout
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <p className="text-center">Your Cart is Empty</p>              
+            </>
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
